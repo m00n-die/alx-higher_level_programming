778cf609c906ed1db9ee8d89fad563b000c65480
moon-die <reginaldrock219@gmail.com>
2023-04-05 18:43:45 -0700

    All mandatory tasks

---
+++ 0x08-python-more_classes/9-rectangle.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+"""A class that defines a rectangle"""
+
+class Rectangle:
+    """represents a rectangle"""
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initializing the rectangle class
+        Args:
+            width: represents the width of the rectangle
+            height: represents the height of the rectangle
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+        self.__width = width
+        self.__height = height
+        type(self).number_of_instances += 1
+
+    def __del__(self):
+        """prints a message for every deleted object"""
+        print("Bye rectangle...")
+        type(self).number_of_instances -= 1
+        
+    @property
+    def width(self):
+        """"returns width attribute"""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """"sets width attribute"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """returns height attribute"""
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """sets height attribute"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self) -> str:
+        """draws a diagram of the rectangle defined for an object"""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        rectangle = ""
+        for column in range(self.__height):
+            for row in range(self.__width):
+                try:
+                    rectangle += str(self.print_symbol)
+                except Exception:
+                    rectangle += type(self).print_symbol
+            if column < self.__height - 1:
+                rectangle += "\n"
+        return (rectangle)
+
+    def __repr__(self):
+        """returns string representation of the class"""
+        return "Rectangle("+ str(self.__width) +", "+ str(self.__height) +")"
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if Rectangle.area(rect_1) > Rectangle.area(rect_2):
+            return rect_1
+        elif Rectangle.area(rect_1) < Rectangle.area(rect_2):
+            return rect_2
+        elif Rectangle.area(rect_1) == Rectangle.area(rect_2):
+            return rect_1
+
+        @classmethod
+        def square(cls, size=0):
+            return cls(size, size)
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+Directory for all projects under 0x08-python-more_classes in the ALX SE Programme
