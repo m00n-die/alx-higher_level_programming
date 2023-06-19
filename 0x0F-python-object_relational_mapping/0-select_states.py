11c4be6784d914d7ff0c29dab7b119501825c909
m00n-die <reginaldrock219@gmail.com>
2023-06-19 09:54:14 -0700

    task 00

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = str(sys.argv[1])
+    password = str(sys.argv[2])
+    dbName = str(sys.argv[3])
+
+    db = MySQLdb.connect(host = "localhost", user = "{}".format(username),
+    password = "{}".format(password), database = "{}".format(dbName))
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
+    result = cur.fetchall()
+    for state in result:
+        print(state)
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+Directory for all files and tasks under 0x0F-python-object_relational_mapping in the ALX SE program
