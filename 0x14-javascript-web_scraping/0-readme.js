35a73f599a9ed29177a087d5e5563eceef6457d7
m00n-die <reginaldrock219@gmail.com>
2023-07-25 17:07:32 +0100

    added encoding argument

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ let filename = (process.argv[2]).toString();
 const fs = require('fs').promises;
 async function readFile(file_name) {
     try {
-        const content = await fs.readFile(file_name);
+        const content = await fs.readFile(file_name, {encoding: 'utf-8'});
         console.log(content.toString());
     } catch (error) {
         console.error(error);
