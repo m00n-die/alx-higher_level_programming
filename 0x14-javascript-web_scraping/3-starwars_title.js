a551f0d560271000c6518893a3b040ca20d595fa
m00n-die <reginaldrock219@gmail.com>
2023-07-29 20:00:46 +0100

    semistandard compliance

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,13 +2,12 @@
 // makes a getrequest to an api
 
 const movieId = process.argv[2];
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 const request = require('request');
 request.get(apiUrl, function (error, response, body) {
   if (!error) {
-    let struct = JSON.parse(body);
+    const struct = JSON.parse(body);
     console.log(struct.title);
-    } else
-      console.log(error);
+  } else { console.log(error); }
 });
