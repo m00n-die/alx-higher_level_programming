3ec69554708d9fce88e4c09cf5d1b5c1d0074d99
m00n-die <reginaldrock219@gmail.com>
2023-07-29 20:50:11 +0100

    task 04

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// print number of movies where character is present
+
+const request = require('request');
+request.get(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const struct = JSON.parse(body).struct;
+    console.log(struct.reduce((count, chars) => {
+      return chars.characters.find((character) => character.endsWith("/18/"))
+        ? count + 1
+        : count;
+    }, 0));
+    } else { console.log(error); }
+});
