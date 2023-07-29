a1706bfa32acbafc142434610952637f4ca491d1
m00n-die <reginaldrock219@gmail.com>
2023-07-29 18:00:53 +0100

    semistandardcompliance met

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 // reads a file
 
-let filename = (process.argv[2]).toString();
+const filename = (process.argv[2]).toString();
 const fs = require('fs').promises;
-async function readFile(file_name) {
-    try {
-        const content = await fs.readFile(file_name, {encoding: 'utf-8'});
-        console.log(content.toString());
-    } catch (error) {
-        console.error(error);
-    }
+async function readFile (file_name) {
+  try {
+    const content = await fs.readFile(file_name, { encoding: 'utf-8' });
+    console.log(content.toString());
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 readFile(filename);
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 // send a get request
 
-let firstArg = process.argv[2];
+const firstArg = process.argv[2];
 
 const request = require('request');
 request.get(firstArg, function (error, response, body) {
-    console.log('code: ', response && response.statusCode);
-    });
+  console.log('code:', response && response.statusCode);
+  console.error('error:', error);
+});
